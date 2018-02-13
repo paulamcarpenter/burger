7a80d5c50167c18f5707db2d60860b1097887912
Paula <paulamcarpenter@gmail.com>
2018-02-13 16:03:16 -0700

    Initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+-- Insert a set of records.
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hot N Spicy", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cool Tomato", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Jacks Back", true);
