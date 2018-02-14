2e7ac7a996a7f972243b7d1cb78543b3bced4ab1
Paula <paulamcarpenter@gmail.com>
2018-02-14 09:40:56 -0700

    Working in Localhost:3000

--- server.js
+++ server.js
@@ -1,36 +1,103 @@
-/ *********************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 var express = require("express");
-// var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
 
-var routes = require("./controllers/burgers_controller.js");
-
-// Sets up the Express App
 var app = express();
-var PORT = process.env.PORT || 8080;
+var port = 3000;
 
-// parse application/x-www-form-urlencoded
+// Parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+var exphbs = require("express-handlebars");
 
-// Set Handlebars.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-app.use(routes);
+var mysql = require("mysql");
 
-// Starts the server to begin listening
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Use Handlebars to render the main index.html page with the todos in it.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { burgers: data });
+  });
+});
+
+// Create a new todo
+app.post("/todos", function(req, res) {
+  connection.query("INSERT INTO burgers (plan) VALUES (?)", [req.body.burgers], function(err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new todo
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
+
+// Retrieve all todos
+app.get("/todos", function(req, res) {
+  connection.query("SELECT * FROM burgers;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.json(data);
+  });
+});
+
+// Update a todo
+app.put("/todos/:id", function(req, res) {
+  connection.query("UPDATE burgers SET burgers = ? WHERE id = ?", [req.body.burgers, req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server faliure
+      return res.status(500).end();
+    }
+    else if (result.changedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+// Delete a todo
+app.delete("/todos/:id", function(req, res) {
+  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+      // If an error occurred, send a generic server faliure
+      return res.status(500).end();
+    }
+    else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+
+  });
+});
+
+app.listen(port, function() {
+  console.log("listening on port", port);
 });
