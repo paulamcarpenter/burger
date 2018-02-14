6d11c6fdf3b75afa82cfe8bb5d62399fbdaecfbf
Paula <paulamcarpenter@gmail.com>
2018-02-14 11:56:00 -0700

    Updating dbase in node

--- server.js
+++ server.js
@@ -1,103 +1,33 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var app = express();
-var port = 3000;
+var express = require("express");
+var bodyParser = require("body-parser");
 
-// Parse application/x-www-form-urlencoded
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
 app.use(bodyParser.json());
 
+// Set Handlebars.
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var mysql = require("mysql");
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burgers_db"
-});
+app.use(routes);
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-
-  console.log("connected as id " + connection.threadId);
-});
-
-// Use Handlebars to render the main index.html page with the todos in it.
-app.get("/", function(req, res) {
-  connection.query("SELECT * FROM burgers;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    res.render("index", { burgers: data });
-  });
-});
-
-// Create a new todo
-app.post("/todos", function(req, res) {
-  connection.query("INSERT INTO burgers (burgers) VALUES (?)", [req.body.burgers], function(err, result) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    // Send back the ID of the new todo
-    res.json({ id: result.insertId });
-    console.log({ id: result.insertId });
-  });
-});
-
-// Retrieve all todos
-app.get("/todos", function(req, res) {
-  connection.query("SELECT * FROM burgers;", function(err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    res.json(data);
-  });
-});
-
-// Update a todo
-app.put("/todos/:id", function(req, res) {
-  connection.query("UPDATE burgers SET burgers = ? WHERE id = ?", [req.body.burgers, req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server faliure
-      return res.status(500).end();
-    }
-    else if (result.changedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-
-  });
-});
-
-// Delete a todo
-app.delete("/todos/:id", function(req, res) {
-  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
-    if (err) {
-      // If an error occurred, send a generic server faliure
-      return res.status(500).end();
-    }
-    else if (result.affectedRows === 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    }
-    res.status(200).end();
-
-  });
-});
-
-app.listen(port, function() {
-  console.log("listening on port", port);
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
 });
