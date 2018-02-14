58f5aaac77cef7a1370703eb021afd5fa1507928
Paula <paulamcarpenter@gmail.com>
2018-02-14 08:21:14 -0700

    update

--- server.js
+++ server.js
@@ -4,31 +4,33 @@
 
 // Dependencies
 // =============================================================
-var express = require("express");
 var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
+var exphbs = require("express-handlebars");
+var express = require("express");
+// var methodOverride = require("method-override");
+
+var routes = require("./controllers/burgers_controller.js");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
-
-// Sets up the Express app to handle data parsing
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-// // Static directory to be served
-// app.use(express.static("app/public"));
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
 
-// // Routes
-// // =============================================================
-// require("./app/routes/api-routes.js")(app);
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+app.use(routes);
 
 // Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
